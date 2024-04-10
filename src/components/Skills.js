6b96bcc71520d6c094eb61b0c52ce210ee731727
codeftshaunak <sagardeyyash19@gmail.com>
2024-04-10 12:00:36 +0600

    update the version

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,19 @@
 import React from "react";
 import styled from "styled-components";
-import { FaHtml5, FaCss3, FaJs, FaReact, FaNode, FaDatabase, FaGithub, FaFigma } from "react-icons/fa";
+import { FaHtml5, FaCss3, FaJs, FaReact, FaNode, FaDatabase, FaGithub, FaFigma, FaAvianex, FaSocks, FaStripe, FaTypo3 } from "react-icons/fa";
 import { SiRedux } from "react-icons/si";
 
 const skillsData = [
   { name: "HTML5", icon: <FaHtml5 /> },
   { name: "CSS3", icon: <FaCss3 /> },
+  { name: "Next.js" },
   { name: "JavaScript", icon: <FaJs /> },
   { name: "React.js", icon: <FaReact /> },
   { name: "Node.js", icon: <FaNode /> },
   { name: "MongoDB", icon: <FaDatabase /> },
   { name: "Github", icon: <FaGithub /> },
+  { name: "Stripe", icon: <FaStripe /> },
+  { name: "TypeScript"},
   { name: "Figma", icon: <FaFigma /> },
   { name: "ES6", icon: <FaJs /> },
   { name: "Redux", icon: <SiRedux /> },
@@ -41,7 +44,7 @@ const SkillCard = styled.div`
   svg {
     width: 50px;
     height: 50px;
-    color: var(--cuteColor);
+   color: var(--cuteColor);
     margin-bottom: 1rem;
     cursor: pointer;
   }
@@ -51,6 +54,10 @@ const SkillCard = styled.div`
     font-weight: 800;
     text-align: center;
     color: var(--cuteColor);
+  }
+
+  img{
+    width:4.2rem;
   }
 `;
 
@@ -63,7 +70,7 @@ const Skills = () => {
       <SkillsContainer>
         {skillsData.map((skill) => (
           <SkillCard key={skill.name}>
-            {skill.icon}
+            {skill.icon&&skill.icon||skill.name=="Next.js"&&<img src="https://cdn.worldvectorlogo.com/logos/next-js.svg"/>||skill.name=="TypeScript"&&<img src="https://static-00.iconduck.com/assets.00/typescript-icon-icon-1024x1024-vh3pfez8.png"/>}
             <h3>{skill.name}</h3>
           </SkillCard>
         ))}
